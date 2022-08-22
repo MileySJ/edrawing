1895f10ff2bd27b0959b9d0f0bc0e7b61ab4cd09
MileySJ <ohmiley007@gmail.com>
2022-08-22 01:32:20 +0100

    art models + static

--- art/models.py
+++ art/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+#from django.contrib.auth.models import user
+from django.conf import settings
 
-# Create your models here.
+
+class Artist(models.Model):
+    account = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.account.username
+
+
+class Art(models.Model):
+    image = models.ImageField()
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=400)
+    price = models.IntegerField()
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
