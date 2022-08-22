527cdee184da6f6bb0cffb4f699de0831541fa82
MileySJ <ohmiley007@gmail.com>
2022-08-22 01:38:22 +0100

    artshop app model

--- art_shop/models.py
+++ art_shop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class ArtSupplies(models.Model):
+    image = models.ImageField()
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=400)
+    price = models.IntegerField()
