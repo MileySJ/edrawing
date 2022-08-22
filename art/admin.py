2d210ac9fd6aee406984d97ebaab0d05f7610921
MileySJ <ohmiley007@gmail.com>
2022-08-22 15:55:45 +0100

    costum django admin for Artist model

--- art/admin.py
+++ art/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 from .models import Artist, Art
+from django.conf import settings
 
 
-admin.site.register(Artist)
+class ArtistDisplay(admin.ModelAdmin):
+    list_display = ('username', 'date_joined', 'last_login',)
+
+    @admin.display(empty_value='unknown')
+    def username(self, obj):
+        return obj.account.username
+    
+    @admin.display(empty_value='unknown')
+    def date_joined(self, obj):
+        return obj.account.date_joined
+
+    @admin.display(empty_value='unknown')
+    def last_login(self, obj):
+        return obj.account.last_login
+
+
+
+
+
+admin.site.register(Artist, ArtistDisplay)
 admin.site.register(Art)
