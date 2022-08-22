40d132812c3ff3d974b4144bb541158ccc2ec6de
MileySJ <ohmiley007@gmail.com>
2022-08-22 01:48:33 +0100

    verbose name plural (model name)

--- art_shop/models.py
+++ art_shop/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
@@ -6,3 +7,7 @@ class ArtSupplies(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=400)
     price = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'Art Supplies'
+        verbose_name_plural = 'Art Supplies'
